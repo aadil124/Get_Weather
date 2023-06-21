cda1d21c3759818010c8b7cd8601fd12432e49f5
aadil124 <ansariaadil124@gmail.com>
2023-06-21 15:44:47 +0530

    weather data on btn click

--- getWeather.js
+++ getWeather.js
@@ -1,7 +1,8 @@
 const restCountryUrl = "https://restcountries.com/v3.1/all";
-
+setTimeout(() => {
+  getDataCountry();
+}, 2000);
 const getDataCountry = async () => {
-
   try {
     const response = await fetch(restCountryUrl);
     const countryData = await response.json();
@@ -12,7 +13,6 @@ const getDataCountry = async () => {
     console.log(err);
   }
 };
-getDataCountry();
 
 const showCountryData = (obj) => {
   const countryName = obj.name.common;
@@ -23,17 +23,54 @@ const showCountryData = (obj) => {
   const flag = obj.flags.png;
   console.log(countryName, region, population);
 
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=88770fab231ef74f4f8ab39995aa3633`;
+
+  const getWeatherData = async (weatherUrl) => {
+    try {
+      const weatherResponse = await fetch(weatherUrl);
+      const weatherData = await weatherResponse.json();
+      const data = weatherData.main;
+      console.log(weatherData.main);
+      const btnWeather = document.querySelector(".btnWeather");
+
+      const handleClick = () => {
+        const pressure = data.pressure;
+        const maxTemp = data.temp_max;
+        const minTemp = data.temp_min;
+        const humidity = data.humidity;
+        console.log("click++++++", pressure, maxTemp, minTemp, humidity);
+        // const pressureDom = document.querySelector(".pressure");
+        // const maxTempDom = document.querySelector(".maxTemp");
+        // const minTempDom = document.querySelector(".minTemp");
+        // const humidityDom = document.querySelector(".humidity");
+        // pressureDom.innerHTML = `Pressure: ${pressure} Pascal`;
+        // maxTempDom.innerHTML = `Max Temp: ${maxTemp - 273} Degree`;
+        // minTempDom.innerHTML = `Min Temp: ${minTemp - 273} Degree`;
+        // humidityDom.innerHTML = `Humidity: ${humidity} `;
+      };
+      btnWeather.addEventListener("click", handleClick);
+    } catch (error) {
+      console.log(`Weather ${error}`);
+    }
+  };
+  getWeatherData(weatherUrl);
+
   const mainContent = document.querySelector(".mainContent");
   mainContent.innerHTML += `
-    <div class="card h-100 bg-dark text-white">
+  <div class="col-sm-12 col-md-4 col-lg-3 mb-3 mb-sm-3">
+      <div class="card h-100 bg-danger text-white">
         <img src="${flag}" class="card-img-top" id="flag" alt="...">
-        <div class="card-body">
-        <h2 class="card-title">${countryName}</h2>
-        <h5 class="card-title">${region}</h5>
-        <h5 class="card-title">${population}</h5>
-        <button class="btn btn-primary">Get Weather</button>
-        <p id="weatherDetails"></p>
-        </div>
-    </div>
+          <div class="card-body">
+            <h2 class="card-title">Name: ${countryName}</h2>
+            <h5 class="card-title">Region: ${region}</h5>
+            <h5 class="card-title">Population: ${population}</h5>
+            <button class="btn btn-primary btnWeather">Get Weather</button>
+            <h5 class="card-title pressure"></h5>
+            <h5 class="card-title maxTemp"></h5>
+            <h5 class="card-title minTemp"></h5>
+            <h5 class="card-title humidity"></h5>
+          </div>
+      </div>
+  </div>
   `;
 };
